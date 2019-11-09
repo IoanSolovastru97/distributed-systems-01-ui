23ba44ada8c81c4f76ccc885fc39175dd87ec153
Ioan Solovastru <34010439+soloman97@users.noreply.github.com>
2019-11-09 15:07:42 +0200

    Asignment02

--- distributed-systems-ui-first/src/app/caregiver/caregiver/caregiver.component.ts
+++ distributed-systems-ui-first/src/app/caregiver/caregiver/caregiver.component.ts
@@ -1,28 +1,67 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, OnDestroy } from '@angular/core';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { StorageService } from 'src/app/shared/services/storage.service';
 import { ToastrService } from 'ngx-toastr';
 import { CaregiverInterface } from 'src/app/shared/models/interfaces/caregiver';
 import { HttpErrorResponse } from '@angular/common/http';
 import { PatientInterface } from 'src/app/shared/models/interfaces/patient';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import { ActivityInterface } from 'src/app/shared/models/interfaces/activity';
 
 @Component({
   selector: 'app-caregiver',
   templateUrl: './caregiver.component.html',
   styleUrls: ['./caregiver.component.scss']
 })
-export class CaregiverComponent implements OnInit {
+export class CaregiverComponent implements OnInit, OnDestroy {
 
-  private caregiverData: CaregiverInterface;
+
+  private caregiverData: CaregiverInterface = new CaregiverInterface();
+  private patients: Array<PatientInterface>;
+  private serverUrl = 'http://localhost:8080/activity';
+  private stompClient;
 
   constructor(
     private apiService: ApiService,
     private storageService: StorageService,
-    private toastr: ToastrService) {
+    private readonly toastr: ToastrService) {
+    this.webSocket(toastr);
   }
 
   ngOnInit() {
     this.getCaregiverDetailsData(this.storageService.get(this.storageService.username));
+
+  }
+  ngOnDestroy(): void {
+    this.stompClient.disconnect();
+  }
+
+  webSocket(toastr) {
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    let activity: ActivityInterface;
+    this.stompClient.connect({}, function () {
+      that.stompClient.subscribe("/topic/activity", (message) => {
+        if (message.body) {
+          activity = JSON.parse(message.body);
+            toastr.info(message.body);
+        }
+      });
+    });
+  }
+
+  checkUsername(activity: ActivityInterface): Boolean {
+    this.patients.forEach(patient => {
+      if (patient.username === activity.username) {
+        console.log("Patient ", patient.username);
+        console.log("Activity ", activity.username);
+        return true;
+      }
+    });
+    console.log("No equal");
+    return false;
   }
 
   /**
@@ -34,6 +73,7 @@ export class CaregiverComponent implements OnInit {
     this.apiService.getCaregiverDetails(username).subscribe(
       (data: CaregiverInterface) => {
         this.caregiverData = data;
+        this.patients = data.patients;
       },
       (error: HttpErrorResponse) => console.error(error)
     );
@@ -41,19 +81,5 @@ export class CaregiverComponent implements OnInit {
   }
 
 
-  // onPatientEdit(patient: PatientInterface) {
-  //   console.log("On patient edit patient = " + patient.username);
-
-  // }
-
-  // onPatientRemove(patient: PatientInterface) {
-  //   console.log("On patient remove patient = " + patient.username);
-  //   this.apiService.getCaregiverDetails(patient.username).subscribe(
-  //     (data: CaregiverInterface) => {
-  //       this.caregiverData = data;
-  //     },
-  //     (error: HttpErrorResponse) => console.error(error)
-  //   );
-  // }
 
 }
